bf51936150c3888d129c38c04f00603cbce43435
biro224 <boubacarbirobarry837@gmail.com>
2023-12-15 01:06:37 +0300

    modification

--- app.js
+++ app.js
@@ -1,7 +1,55 @@
+const socket = require('socket.io')
 const express = require('express')
 const path = require('path')
-const app =  express()
+const app = express()
+
 const PORT = process.env.PORT || 4000
 const server = app.listen(PORT, () => console.log(`💬 server on port ${PORT}`))
 
+const io = require('socket.io')(server)
+
 app.use(express.static(path.join(__dirname, 'public')))
+
+let socketsConected = new Set()
+
+io.on('connection', onConnected)
+
+function onConnected(socket) {
+  console.log(socket.id)
+  socketsConected.add(socket.io)
+
+  io.emit('clients-total', socketsConected.size)
+
+  socket.on('disconnect', () => {
+    console.log('socket disconnected',socket.id)
+    socketsConected.delete(socket.id)
+    io.emit('clients-total', socketsConected.size)
+  })
+}
+
+/**
+ * 
+ * 
+ * 
+
+function onConnected(socket) {
+  console.log('Socket connected', socket.id)
+  socketsConected.add(socket.id)
+  io.emit('clients-total', socketsConected.size)
+
+  socket.on('disconnect', () => {
+    console.log('Socket disconnected', socket.id)
+    socketsConected.delete(socket.id)
+    io.emit('clients-total', socketsConected.size)
+  })
+
+  socket.on('message', (data) => {
+    // console.log(data)
+    socket.broadcast.emit('chat-message', data)
+  })
+
+  socket.on('feedback', (data) => {
+    socket.broadcast.emit('feedback', data)
+  })
+}
+ */
