bfda130f7781f0f9dde0a24c4018bacd918913c2
biro224 <boubacarbirobarry837@gmail.com>
2023-12-21 13:01:36 +0300

    feedback

--- public/main.js
+++ public/main.js
@@ -7,7 +7,7 @@ const nameInput = document.getElementById('name-input')
 const messageForm = document.getElementById('message-form')
 const messageInput = document.getElementById('message-input')
 
-messageForm.addEventListener('submit',(e) =>{
+messageForm.addEventListener('submit', (e) => {
     e.preventDefault()
     sendMessage()
 })
@@ -16,8 +16,9 @@ socket.on('clients-total', (data) => {
     clientsTotal.innerText = `Total Clients: ${data}`
 })
 
-function  sendMessage(){
-    console.log(messageInput.value)
+function sendMessage() {
+    if (messageInput.value === '') return
+    // console.log(messageInput.value)
     const data = {
         name: nameInput.value,
         message: messageInput.value,
@@ -25,9 +26,32 @@ function  sendMessage(){
 
     }
 
-    socket.emit('message',data)
+    socket.emit('message', data)
+    addMessageToUI(true, data)
+    messageInput.value = ''
 }
 
-socket.on('chat-message',(data)=>{
+socket.on('chat-message', (data) => {
     console.log(data)
+    addMessageToUI(false, data)
+    addMessageToUI(true, data)
+    messageInput.value = ''
 })
+
+function addMessageToUI(isOwnMessage, data) {
+    const element = `
+        <li class="${isOwnMessage ? "message-right" : "message-left"} ">
+            <p class="message">
+                ${data.message}
+                <span> ${data.name} ● ${moment(data.dateTime).fromNow()}</span>
+            </p>
+        </li>`
+
+    messageContainer.innerHTML += element
+
+    scrollToBottom()
+}
+
+function scrollToBottom() {
+    messageContainer.scrollTo(0, messageContainer.scrollHeight)
+}
